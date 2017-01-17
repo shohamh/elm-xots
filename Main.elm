6d359fd9cd4aa262f2fdde99eb1bb0573bc5de40
shohamh <shohamh@hotmail.com>
2017-01-17 15:43:53 +0200

    initial commit

---
+++ Main.elm
@@ -0,0 +1,94 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Events exposing (onClick)
+import Basics exposing (toString)
+
+
+-- MODEL
+
+
+type alias Model =
+    { currentValue : Int
+    }
+
+
+model : Model
+model =
+    { currentValue = 0
+    }
+
+
+type Msg
+    = NoOp
+    | ButtonClick
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        NoOp ->
+            model
+
+        ButtonClick ->
+            { model | currentValue = model.currentValue + 1 }
+
+
+
+-- VIEW
+
+
+ListOfHtmlElements : Int -> Html Msg -> Html Msg
+generateRowInTable n element =
+    let
+        row =
+            []
+    in
+        case List.length row of
+            n ->
+                row
+
+            _ ->
+                element :: row
+
+
+generateTable : Int -> Int -> Html Msg -> Html Msg
+generateTable col row element =
+    let
+        table =
+            []
+    in
+        case List.length table of
+            col ->
+                table
+
+            _ ->
+                (generateRowInTable row element) :: table
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick ButtonClick ]
+            [ text
+                ("currentValue: "
+                    ++ toString model.currentValue
+                )
+            ]
+        ]
+
+
+
+-- APP
+
+
+main =
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
