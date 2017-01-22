b07868d4a8f6e8aae9813ac7a4bf1f4edfec984b
shohamh <shohamh@hotmail.com>
2017-01-22 16:24:01 +0200

    Added logic of xots game, still not working though

--- Main.elm
+++ Main.elm
@@ -2,26 +2,52 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Events exposing (onClick)
-import Basics exposing (toString)
+import Html.Attributes exposing (id)
+import Basics exposing (toString, floor, negate)
+import List exposing (..)
+import Dict exposing (..)
+import Maybe
+import Maybe.Extra exposing (join)
+import Debug exposing (log)
+import Tuple exposing (..)
 
 
 -- MODEL
 
 
 type alias Model =
-    { currentValue : Int
+    { board : Dict ( Int, Int ) Player
+    , currentPlayer : Player
+    , width : Int
+    , height : Int
+    , winner : Maybe Player
+    , lastMove : ( Int, Int )
+    , chainToWin : Int
     }
 
 
 model : Model
 model =
-    { currentValue = 0
+    { board = Dict.empty
+    , currentPlayer = X
+    , width = 10
+    , height = 10
+    , winner = Nothing
+    , lastMove = ( 0, 0 )
+    , chainToWin = 5
     }
 
 
 type Msg
     = NoOp
-    | ButtonClick
+    | ButtonClick ( Int, Int )
+
+
+type Player
+    = X
+    | O
+    | T
+    | S
 
 
 
@@ -34,55 +60,230 @@ update msg model =
         NoOp ->
             model
 
-        ButtonClick ->
-            { model | currentValue = model.currentValue + 1 }
+        ButtonClick ( x, y ) ->
+            { model
+                | board = Dict.insert ( x, y ) model.currentPlayer model.board
+                , currentPlayer =
+                    nextPlayer
+                        model.currentPlayer
+                , winner =
+                    checkWin
+                        { model
+                            | board =
+                                Dict.insert ( x, y )
+                                    model.currentPlayer
+                                    model.board
+                        }
+                , lastMove = ( x, y )
+            }
+
+
+checkWin : Model -> Maybe Player
+checkWin model =
+    let
+        filterLastPlayer : ( Int, Int ) -> Player -> Bool
+        filterLastPlayer ( x, y ) player =
+            if player == prevPlayer model.currentPlayer then
+                True
+            else
+                False
+
+        filteredBoard =
+            Dict.filter filterLastPlayer model.board
+
+        longestChainRow =
+            lengthOfLongestPartialListOfDefinedElements <|
+                getRowFromBoard filteredBoard model.lastMove model.chainToWin
+
+        longestChainCol =
+            lengthOfLongestPartialListOfDefinedElements <|
+                getColFromBoard filteredBoard model.lastMove model.chainToWin
+
+        longestChainDiagonal =
+            lengthOfLongestPartialListOfDefinedElements <|
+                getDiagonalFromBoard filteredBoard model.lastMove model.chainToWin
+
+        longestChainAntiDiagonal =
+            lengthOfLongestPartialListOfDefinedElements <|
+                getAntiDiagonalFromBoard filteredBoard model.lastMove model.chainToWin
+    in
+        if
+            longestChainRow
+                >= model.chainToWin
+                || longestChainCol
+                >= model.chainToWin
+                || longestChainDiagonal
+                >= model.chainToWin
+                || longestChainAntiDiagonal
+                >= model.chainToWin
+        then
+            Debug.log
+                (toString longestChainRow
+                    ++ ","
+                    ++ toString longestChainCol
+                    ++ ","
+                    ++ toString longestChainDiagonal
+                    ++ ","
+                    ++ toString longestChainAntiDiagonal
+                )
+                Just
+                (prevPlayer model.currentPlayer)
+        else
+            Debug.log
+                (toString longestChainRow
+                    ++ ","
+                    ++ toString longestChainCol
+                    ++ ","
+                    ++ toString longestChainDiagonal
+                    ++ ","
+                    ++ toString longestChainAntiDiagonal
+                )
+                Nothing
+
+
+lengthOfLongestPartialListOfDefinedElements : List (Maybe a) -> Int
+lengthOfLongestPartialListOfDefinedElements list =
+    case list of
+        [] ->
+            0
+
+        Nothing :: rest ->
+            0
+
+        (Just _) :: rest ->
+            1 + lengthOfLongestPartialListOfDefinedElements rest
+
+
+getRowFromBoard : Dict ( Int, Int ) Player -> ( Int, Int ) -> Int -> List (Maybe Player)
+getRowFromBoard filteredBoard lastMove chainToWin =
+    List.map
+        (\x ->
+            Dict.get ( first lastMove + x, second lastMove )
+                filteredBoard
+        )
+    <|
+        List.range (negate chainToWin) chainToWin
+
+
+getColFromBoard : Dict ( Int, Int ) Player -> ( Int, Int ) -> Int -> List (Maybe Player)
+getColFromBoard filteredBoard lastMove chainToWin =
+    List.map
+        (\x ->
+            Dict.get ( first lastMove, second lastMove + x )
+                filteredBoard
+        )
+    <|
+        List.range (negate chainToWin) chainToWin
+
+
+getDiagonalFromBoard : Dict ( Int, Int ) Player -> ( Int, Int ) -> Int -> List (Maybe Player)
+getDiagonalFromBoard filteredBoard lastMove chainToWin =
+    List.map
+        (\x ->
+            Dict.get ( first lastMove + x, second lastMove + x )
+                filteredBoard
+        )
+    <|
+        List.range (negate chainToWin) chainToWin
+
+
+getAntiDiagonalFromBoard : Dict ( Int, Int ) Player -> ( Int, Int ) -> Int -> List (Maybe Player)
+getAntiDiagonalFromBoard filteredBoard lastMove chainToWin =
+    List.map (\x -> Dict.get ( first lastMove + x, second lastMove + x ) filteredBoard) <|
+        List.reverse <|
+            List.range (negate chainToWin) chainToWin
+
+
+nextPlayer : Player -> Player
+nextPlayer player =
+    case player of
+        X ->
+            O
+
+        O ->
+            T
+
+        T ->
+            S
+
+        S ->
+            X
+
+
+prevPlayer : Player -> Player
+prevPlayer player =
+    case player of
+        X ->
+            S
+
+        O ->
+            X
+
+        T ->
+            O
+
+        S ->
+            T
 
 
 
 -- VIEW
 
 
-generateRowInTable : Int -> Html Msg -> Html Msg
-generateRowInTable n element =
-    let
-        row =
-            []
-    in
-        tr []
-            [ case List.length row of
-                n ->
-                    row
+printPlayer : Maybe Player -> Html Msg
+printPlayer player =
+    case player of
+        Nothing ->
+            text ""
 
-                _ ->
-                    td [] [ element ] :: row
+        Just X ->
+            text "X"
+
+        Just O ->
+            text "O"
+
+        Just T ->
+            text "T"
+
+        Just S ->
+            text "S"
+
+
+makeTdWithButton : Model -> ( Int, Int ) -> Html Msg
+makeTdWithButton model ( x, y ) =
+    td []
+        [ button [ onClick (ButtonClick ( x, y )) ]
+            [ printPlayer <| Dict.get ( x, y ) model.board
             ]
+        ]
 
 
-generateTable : Int -> Int -> Html Msg -> Html Msg
-generateTable col row element =
-    let
-        listOfRows =
-            []
-    in
-        table []
-            [ case List.length listOfRows of
-                col ->
-                    listOfRows
+generateRow : Model -> Int -> Int -> Html Msg
+generateRow model start length =
+    tr []
+        (List.map (makeTdWithButton model) <|
+            List.map (\n -> ( start, n )) <|
+                List.range 1 length
+        )
 
-                _ ->
-                    (generateRowInTable row element) :: listOfRows
-            ]
+
+showBoard : Model -> Html Msg
+showBoard model =
+    table []
+        (List.map2 (generateRow model)
+            (List.map (\x -> x * model.width)
+                (List.range 1 model.height)
+            )
+            (List.repeat model.height model.width)
+        )
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick ButtonClick ]
-            [ text
-                ("currentValue: "
-                    ++ toString model.currentValue
-                )
-            ]
+        [ showBoard
+            model
+        , printPlayer model.winner
         ]
 
 
